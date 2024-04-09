2c4575994b9799a338eaaaf470ac0cdf33337470
VIJAYaraaghavan Manoharan <AMVijay@users.noreply.github.com>
2024-04-09 11:27:17 +0530

    spring rest api timeout implementation

---
+++ spring-rest-api-timeout/src/main/java/com/amvijay/springrestapitimeout/SpringRestApiTimeoutApplication.java
@@ -0,0 +1,13 @@
+package com.amvijay.springrestapitimeout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestApiTimeoutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestApiTimeoutApplication.class, args);
+	}
+
+}
